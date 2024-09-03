45ffd605b19d32aa7a1279940ccbed950fce667a
pratikthoriya2204 <pratikcollege123@gmail.com>
2024-09-03 21:04:55 +0530

    ODD even Program

---
+++ P2_Odd_Even.java
@@ -0,0 +1,11 @@
+public class P2_Odd_Even {
+    public static void main(String[] args) {
+        int num=4;
+
+        if(num % 2 == 0){
+            System.out.println("EVEN");
+        }else{
+            System.out.println("ODD");
+        }
+    }
+}
