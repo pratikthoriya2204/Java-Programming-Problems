7ec3ba0492a8b2d99706b8f64d665d33a23eaab1
pratikthoriya2204 <pratikcollege123@gmail.com>
2024-09-03 20:20:48 +0530

    sum of Natural number

---
+++ P1_sum_Natural_numbers.java
@@ -0,0 +1,14 @@
+/**
+ * P1_sum_Natural_numbers
+ */
+public class P1_sum_Natural_numbers {
+
+    public static void main(String[] args) {
+        int i,num=15,sum=0;
+
+        for(i=1;i<=num;i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
+    }
+}
