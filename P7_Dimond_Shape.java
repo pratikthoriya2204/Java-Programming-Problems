41a63a06870425bfb1747d13f68dbfcfb7d2744e
pratikthoriya2204 <pratikcollege123@gmail.com>
2024-09-04 14:57:13 +0530

    Dimond Shape

---
+++ P7_Dimond_Shape.java
@@ -0,0 +1,24 @@
+public class P7_Dimond_Shape {
+    public static void main(String[] args) {
+        int i,j;
+
+        for (i=1;i<=5;i++){
+            for (int k=1;k<= 5-i;k++){
+                System.out.print(" ");
+            }
+            for(j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for (i=5;i>=1;--i){
+            for (int k=1;k<=5-i;k++){
+                System.out.print(" ");
+            }
+            for (j=1;j<=i;j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+}
